ae05670949830bb76b8858b9c3d5395494ee6cf0
Yunus Kazimov <yunus.kazimov@gmail.com>
2022-02-07 19:26:11 +0400

    task2: edit k8s files and fix bugs

--- src/main/java/az/xazar/msauthamigo/filter/CustomAuthenticationFilter.java
+++ src/main/java/az/xazar/msauthamigo/filter/CustomAuthenticationFilter.java
@@ -63,11 +63,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         response.setHeader("access_token", access_token);
         response.setHeader("refresh_token", refresh_token);
-
-//        Map<String, String> tokens = new HashMap<>();
-//        tokens.put("access_token", access_token);
-//        tokens.put("refresh_token", refresh_token);
-//        response.setContentType(APPLICATION_JSON_VALUE);
-//        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
+
 }
